a24ba718675ddd765645cba20855ef90c67ad357
Carlita <carlareyesgo@gmail.com>
2020-12-09 15:15:41 -0700

    Styled-button

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,6 +5,13 @@ const blog = () => {
   return (
     <Layout>
       <h1>This is our blog page</h1>
+      <p className="blog-text">
+        Consequat laborum in duis anim aute qui. Nulla commodo laborum enim
+        commodo consequat ut quis occaecat enim non non exercitation. Non minim
+        adipisicing duis Lorem occaecat reprehenderit nulla aliqua mollit amet
+        eu culpa. Ea magna deserunt eiusmod cillum. Ullamco laborum aute non
+        quis id ad in duis sunt adipisicing velit velit enim.
+      </p>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -6,6 +6,13 @@ export default class products extends Component {
     return (
       <Layout>
         <h1>This is our products</h1>
+        <p className="products-text">
+          Consequat laborum in duis anim aute qui. Nulla commodo laborum enim
+          commodo consequat ut quis occaecat enim non non exercitation. Non
+          minim adipisicing duis Lorem occaecat reprehenderit nulla aliqua
+          mollit amet eu culpa. Ea magna deserunt eiusmod cillum. Ullamco
+          laborum aute non quis id ad in duis sunt adipisicing velit velit enim.
+        </p>
       </Layout>
     )
   }
