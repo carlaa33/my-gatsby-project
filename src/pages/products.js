c32e2d00d06a33e542f7556ec150bb6ebf251794
Carlita <carlareyesgo@gmail.com>
2020-12-09 13:33:15 -0700

    first commit

---
+++ src/pages/products.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import Layout from "../components/layout"
+
+export default class products extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>This is our products</h1>
+      </Layout>
+    )
+  }
+}
