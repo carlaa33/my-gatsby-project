0f1ee37304bea1c2a93f71fa067d5e70c1a7b652
Carlita <carlareyesgo@gmail.com>
2020-12-10 11:16:21 -0700

    useStaticQuery and Graphql

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,10 +1,38 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        person {
+          age
+          name
+        }
+        author
+        data
+        description
+        title
+      }
+    }
+  }
+`
 const Header = () => {
+  const {
+    site: {
+      siteMetadata: {
+        title,
+        person: { name },
+      },
+    },
+  } = useStaticQuery(getData)
+
   return (
     <div>
-      <h1>This is our heading</h1>
+      {/*<h1> title: {data.site.siteMetadata.title} </h1>*/}
+      {/*<h1> name: {data.site.siteMetadata.person.name} </h1>*/}
+      <h1>title: {title}</h1>
+      <h1>name: {name}</h1>
     </div>
   )
 }
