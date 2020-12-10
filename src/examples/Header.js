d3d61bfbffe94b9a509a47995857b14294b534f1
Carlita <carlareyesgo@gmail.com>
2020-12-10 10:38:31 -0700

    graphql

---
+++ src/examples/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Header = () => {
+  return (
+    <div>
+      <h1>This is our heading</h1>
+    </div>
+  )
+}
+
+export default Header
