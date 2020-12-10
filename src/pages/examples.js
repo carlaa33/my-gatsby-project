4314d71b34feea40682de34644ea3bdbff5eceaf
Carlita <carlareyesgo@gmail.com>
2020-12-10 11:35:26 -0700

    StaticQuery Component

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "../examples/Header"
 import Layout from "../components/layout"
+import HeaderStatic from "../examples/HeaderStatic"
 
 const examples = () => {
   return (
     <Layout>
       <h1>Hello from examples page</h1>
       <Header />
+      <HeaderStatic />
     </Layout>
   )
 }
