9d20d3caaf88b73f3c0a0d620b6a0cd9e57d6aee
Carlita <carlareyesgo@gmail.com>
2020-12-11 12:46:02 -0700

    Gatsby-Node

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -1,6 +1,7 @@
 import React from "react"
 
-const productTemplate = () => {
+const productTemplate = props => {
+  console.log(props)
   return (
     <div>
       <h1>hello from product template</h1>
