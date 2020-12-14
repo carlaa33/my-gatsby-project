b3a9ca85c5a3d923337c3cfccdd5cf9e0424d256
Carlita <carlareyesgo@gmail.com>
2020-12-14 13:26:34 -0700

    Connect Gatsby-Strapi

--- Front/gatsby-config.js
+++ Front/gatsby-config.js
@@ -41,5 +41,13 @@ module.exports = {
         accessToken: process.env.ACCESS_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`jobs`],
+      },
+    },
   ],
 }
